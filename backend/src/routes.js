6673dfa3bfef5594388321526fcd85ee1e23f762
FaraoAten <atenfernandes@gmail.com>
2020-10-05 10:46:27 -0300

    Criação rota registro do usuario, peq erro no bd

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,9 +1,21 @@
 const express = require('express');
 
+const connection = require('./database/connection');
+
 const routes = express.Router();
 
-routes.get('/', (request,response) => {
-    return response.send('Hello World');
+routes.post('/usuario', async(request,response) => {
+    const {num_matricula, email, senha, nome, tipo} = request.body;
+
+    await connection('usuario').insert({
+        num_matricula, 
+        email, 
+        senha, 
+        nome, 
+        tipo
+    })
+
+    return response.json({email, senha}); //essa resposta só para testes.
 });
 
 module.exports = routes; 
