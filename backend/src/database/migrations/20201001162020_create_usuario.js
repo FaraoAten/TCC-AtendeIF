9729c9ef6ce5b003e8ff3c13121fe24c02cf19fb
FaraoAten <atenfernandes@gmail.com>
2021-03-30 21:44:58 -0300

    atualização bd

--- backend/src/database/migrations/20201001162020_create_usuario.js
+++ backend/src/database/migrations/20201001162020_create_usuario.js
@@ -3,7 +3,6 @@ exports.up = function(knex) {
   return knex.schema.createTable('usuario', function(table){
       table.string('id_usuario').primary().notNullable().unique();
       table.string('num_matricula').notNullable().unique();
-      table.string('email').notNullable().unique();
       table.string('senha').notNullable();
       table.string('nome').notNullable();
       table.integer('tipo',1).notNullable();
